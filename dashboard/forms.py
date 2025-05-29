030a6b9cdce2630b6b7e8814290521a5ca8b208f
YOGEHWAR NIRMALE <nirmaleyogesh050@gmail.com>
2025-05-29 14:24:46 +0530

    Update forms.py

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.forms import widgets
 from .models import *
-from django.contrib.auth.forms import UserCreationForm  # ✅
+from django.contrib.auth.forms import UserCreationForm  
 
 
 class NotesForm(forms.ModelForm):
